a8609e471f30d9460a950dd570e58d84a8142834
栗田慎之介 <shinnosuke.kurita@nide.jp>
2024-01-21 18:21:07 +0900

    modified

---
+++ pages/[...slug].vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const search  = ref('')
+
+const results = searchContent(search)
+
+</script>
+
+<template>
+  <main>
+    <!-- <input v-model="search">
+    <pre>{{ results }} </pre> -->
+    <!-- <ContentDoc/> -->
+    <ContentDoc v-slot="{ doc }">
+      <article>
+        <h1>{{ doc.title }}</h1>
+        <ContentRenderer :value="doc" />
+      </article>
+    </ContentDoc>
+    
+  </main>
+</template>
+
+
